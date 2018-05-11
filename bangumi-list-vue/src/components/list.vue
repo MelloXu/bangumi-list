1294714b484d70427c5d34758ef407b61afd0125
yukinya <xiaoximiao0123@gmail.com>
2018-05-11 20:24:50 +0800

    add vue project

---
+++ bangumi-list-vue/src/components/list.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="card-list">
+        <div class="item" v-for="(item,index) in listArray" :key="index">
+            <div class="content">
+                <div class="embed-responsive">
+                    <div class="cover">
+                        <img v-lazy="item.img_src" alt="">
+                    </div>
+                </div>
+                <div class="info">
+                    <h3 class="title">{{item.name}}</h3>
+                    <div class="stat">
+                        <a class="detail" :href="item.link">查看详情</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+          listArray: []
+      }
+  },
+  created() {},
+  mounted() {
+      this.getList();
+  },
+  methods: {
+      getList: function() {
+          this.axios.get('./static/api/list').then((res)=> {
+             var info = res.data.list;
+             this.listArray = info;
+          });
+      }
+  }
+}
+</script>
+
+
---
+++ bangumi-list-vue/src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+import VueLazyLoad from 'vue-lazyload'
+
+Vue.config.productionTip = false
+Vue.prototype.axios = axios
+
+Vue.use(VueLazyLoad, {
+    preLoad: 1.3,
+    error: '/assets/img/loaderror.png',
+    loading: '/assets/img/lazyload.png',
+    attempt: 1
+});
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
