c8d2a8d7285405c773e8244c1fab59ef5dc112c3
yukinya <xiaoximiao0123@gmail.com>
2018-05-14 20:00:28 +0800

    test

--- bangumi-list-vue/src/App.vue
+++ bangumi-list-vue/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    testststtstststt
     <router-view></router-view>
   </div>
 </template>
--- bangumi-list-vue/src/router/index.js
+++ bangumi-list-vue/src/router/index.js
@@ -5,6 +5,7 @@ Vue.use(Router)
 
 export default new Router({
   mode: 'history',
+  base: __dirname,
   routes: [
     {
       path: '/',
