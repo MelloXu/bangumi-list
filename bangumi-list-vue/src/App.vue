ce00287399fce3eb98cf0e642bede2c64ff92c47
yukinya <xiaoximiao0123@gmail.com>
2018-05-15 21:09:37 +0800

    update

--- bangumi-list-vue/src/App.vue
+++ bangumi-list-vue/src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
+
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import header from '@/components/header'
 export default {
-  name: 'App'
+  data() {
+
+  },
+  components: {
+
+  }
 }
 </script>
 
---
+++ bangumi-list-vue/src/components/header.vue
@@ -0,0 +1,5 @@
+<template>
+    <header id="header">
+        ゆき的补番列表(๑╹ω╹๑ )
+    </header>
+</template>
